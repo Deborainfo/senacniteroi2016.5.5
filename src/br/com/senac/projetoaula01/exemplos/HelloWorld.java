4d946739d0dfb6e0fe56ff79ee9919db6e1651ff
51115152017.2 <51115152017.2@51DE194272.senacrjedu.local>
2017-08-25 16:56:48 -0300

    Primeiro Commint

---
+++ src/br/com/senac/projetoaula01/exemplos/HelloWorld.java
@@ -0,0 +1,9 @@
+package br.com.senac.projetoaula01.exemplos;
+//cabeçalho de declaração da classe
+public class HelloWorld {
+
+	// Método que Executa sua Aplicação
+	public static void main(String[] args){
+		System.out.println("Hello Java World!");
+	}
+}
